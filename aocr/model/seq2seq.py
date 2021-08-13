402f8e3c9e1aa0291eef4a0bf5c8edf4350dea77
devershichandra27 <devershichandra27@gmail.com>
2021-08-14 02:13:50 +0530

    hidden tfprint

--- aocr/model/seq2seq.py
+++ aocr/model/seq2seq.py
@@ -192,8 +192,9 @@ def attention_decoder(decoder_inputs, initial_state, attention_states, cell,
         for a in xrange(num_heads):
             k = tf.get_variable("AttnW_%d" % a,
                                 [1, 1, attn_size, attention_vec_size])
-            k = tf.Print(k, [k], "$K", summarize=512)
+            # k = tf.Print(k, [k], "$K", summarize=512)
             hidden = tfprint(hidden,'D_HIDDEN')
+            hidden = tf.Print(hidden, [hidden], "$hidden", summarize=262144)
             conv_d = tf.nn.conv2d(hidden, k, [1, 1, 1, 1], "SAME")
             conv_d = tfprint(conv_d, 'D_CONV')
             hidden_features.append(conv_d)
